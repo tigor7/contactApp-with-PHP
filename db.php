195a0c5d257d737c948f1700ae629e492f077302
Santiago <santiagopujol7@gmail.com>
2020-12-26 22:47:25 +0000

    Proyect start

---
+++ db.php
@@ -0,0 +1,11 @@
+<?php 
+	
+	try {
+		$db = new PDO("mysql:host=localhost; dbname=contactapp", "root", "");
+	
+
+	} catch (Exception $e){}
+
+
+
+ ?>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- BOOTSTRAP CSS -->
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+	<!-- FONT AWESOME SCRIPTS -->
+	<script src="https://kit.fontawesome.com/49b6ed66b0.js" crossorigin="anonymous"></script>
+	<title>contactApp with PHP</title>
+</head>
+<body>
+
+	<nav class="navbar navbar-dark bg-dark">
+		<div class="container">
+			<a href="index.php" class="navbar-brand">contactApp with PHP</a>
+		</div>
+	</nav>
